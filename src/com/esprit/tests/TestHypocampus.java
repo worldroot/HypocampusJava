5b12c985f87e162dabb18636eecc6e235dd13848
Mehdi Behira <mehdibehira@gmail.com>
2020-03-07 12:08:35 +0100

    first commit

---
+++ src/com/esprit/tests/TestHypocampus.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.tests;
+
+/**
+ *
+ * @author mehdibehira
+ */
+public class TestHypocampus {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("test");
+               
+    }
+
+    
+}
